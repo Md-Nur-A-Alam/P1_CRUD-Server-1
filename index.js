6e4ef350261ec26b2169ce5618c9266c8a03a54f
Md. Nur A Alam <mdnuralam2812@gmail.com>
2026-06-25 15:36:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ServerDescription } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-mk4quir-shard-00-00.8gsm7ez.mongodb.net:27017,ac-mk4quir-shard-00-01.8gsm7ez.mongodb.net:27017,ac-mk4quir-shard-00-02.8gsm7ez.mongodb.net:27017/?ssl=true&replicaSet=atlas-2qwr4j-shard-0&authSource=admin&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const run = async () => {
+    try {
+        // Connect the client to the server
+        await client.connect();
+
+        const db = client.db('CRUD-1');
+        const usersCollection = db.collection('users');
+
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
